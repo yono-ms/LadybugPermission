adfdfa4390dada4f8cbfe3f15b7bfccbd0c4eba9
yono.ap <yono.ap@icloud.com>
2025-02-23 10:02:44 +0900

    SLF4J

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.slf4j.api)
+    implementation(libs.logback.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/ladybugpermission/MainActivity.kt
+++ app/src/main/java/com/example/ladybugpermission/MainActivity.kt
@@ -6,10 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.ladybugpermission.ui.screen.MainScreen
 import com.example.ladybugpermission.ui.theme.LadybugPermissionTheme
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        loggerTest()
         enableEdgeToEdge()
         setContent {
             LadybugPermissionTheme {
@@ -17,4 +20,14 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    private fun loggerTest() {
+        logger.trace("Logger TEST")
+        logger.debug("Logger TEST")
+        logger.info("Logger TEST")
+        logger.warn("Logger TEST")
+        logger.error("Logger TEST")
+    }
 }
+
+val logger: Logger by lazy { LoggerFactory.getLogger("LadybugP") }
