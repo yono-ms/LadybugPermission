703177eaebb99f865388732bc2ec8545208de892
yono.ap <yono.ap@icloud.com>
2025-02-24 15:29:50 +0900

    Google Play Service Location.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.slf4j.api)
     implementation(libs.logback.android)
+    implementation(libs.play.services.location)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
